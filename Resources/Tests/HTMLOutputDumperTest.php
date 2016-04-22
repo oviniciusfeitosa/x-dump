093e6de7d96a74c64bf33a1373c6b7c053f57a38
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2016-04-22 16:48:58 +0000

    [add] Including the initial structure of component

---
+++ Resources/Tests/HTMLOutputDumperTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
+ * Date: 22/04/2016
+ * Time: 13:35
+ */
+
+namespace Component\xDump\Test;
+
+class HTMLOutputDumperTest extends \PHPUnit_Framework_TestCase {
+    public function testOutput() {
+        /**
+        * @todo : Fill this guy
+        */
+    }
+}
